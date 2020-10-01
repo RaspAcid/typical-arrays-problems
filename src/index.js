1733b9881d3af2e4f73e3aad2b7e840e78740fca
RaspAcid <rasp.acid@gmail.com>
2020-10-01 18:44:27 +0300

    feat: add new functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (typeof array === 'undefined') return 0;
+    if (array.length === 0) return 0;
+    let num = array[0];
+    let i = 0;
+    while (i < array.length) {
+        if (num < array[i]) {
+            i = i + 1;
+        } else {
+            num = array[i];
+            i = i + 1;
+        }
+    }
+  return num;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array === 'undefined') return 0;
+    if (array.length === 0) return 0;
+    let num = array[0];
+    let i = 0;
+    while (i < array.length) {
+        if (num > array[i]) {
+            i = i + 1;
+        } else {
+            num = array[i];
+            i = i + 1;
+        }
+    }
+  return num;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array === 'undefined') return 0;
+    if (array.length === 0) return 0;
+    let sum = 0;
+    let i = 0;
+    while (i < array.length) {
+        sum = array[i] + sum;
+        i = i + 1;
+    }
+    let avg = sum / array.length;
+  return avg;
 }
